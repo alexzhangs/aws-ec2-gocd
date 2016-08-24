1d371c0734bd8f05e643bca01d8a5051d0afa6af
alex <alexzhangs@gmail.com>
2016-08-24 10:23:28 +0800

    Add ln -s to install.sh

--- install.sh
+++ install.sh
@@ -8,8 +8,11 @@ find "$dir" -type f -not -name $(basename "$0") -and \( -name "*.sh" -or -name "
         echo "  $f"
         fn=$(basename "$f") && \
             /bin/cp -a "$f" /usr/local/bin/ && \
-            /bin/chmod 755 /usr/local/bin/$fn
-    done || exit $?
+            /bin/chmod 755 /usr/local/bin/$fn && \
+            ( if echo "$fn" | grep -q '\.sh$'; then /bin/ln -sf "$fn" /usr/local/bin/${fn%.sh}; fi )
+    done
+
+ret=$(( PIPESTATUS + $? ))
 
 echo ">>DONE."
-exit
+exit $ret
